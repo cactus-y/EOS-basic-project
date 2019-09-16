84730dcaa1fc5116cfcdc5f9df4fe9f519ddc651
2019032160 <2019032160@hanyang.ac.kr>
2019-09-16 19:28:47 +0900

    room, rcv

--- app/src/main/java/com/example/eos_basic_todo/main/MainActivity.java
+++ app/src/main/java/com/example/eos_basic_todo/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.eos_basic_todo;
+package com.example.eos_basic_todo.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.eos_basic_todo.R;
 
 public class MainActivity extends AppCompatActivity {
 
